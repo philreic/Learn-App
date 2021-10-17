32a76ed9bb65e0137f6a642e7cd732ffbe3fc3a4
philreic <91380439+philreic@users.noreply.github.com>
2021-10-17 13:44:26 -0700

    Initial Commit

---
+++ Learn App/Learn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_AppApp.swift
+//  Learn App
+//
+//  Created by Philippe Reichen on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
