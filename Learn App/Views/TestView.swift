d15c194edfdb824cb86d7ca7e481e863e42ab04b
philreic <91380439+philreic@users.noreply.github.com>
2021-10-20 10:00:43 -0700

    Buf fix Xcode 12.5

--- Learn App/Views/TestView.swift
+++ Learn App/Views/TestView.swift
@@ -15,11 +15,11 @@ struct TestView: View {
     @State var submitted = false
     
     @State var numCorrect = 0
-    
+    @State var showResults = false
     
     var body: some View {
         
-        if model.currentQuestion != nil {
+        if model.currentQuestion != nil && showResults == false {
             
             VStack(alignment: .leading){
                 // Question number
@@ -99,12 +99,22 @@ struct TestView: View {
                     // Check if answer has been submitted
                     if submitted == true {
                         
-                        // Answer has already been submitted  move to next quetsion
-                        model.nextQuestion()
+                        // Check if its the last question
+                        if model.currentQuestionIndex + 1 == model.currentModule!.test.questions.count {
+                            
+                            // Show the results
+                            showResults = true
+                        }
+                        else{
+                            // Answer has already been submitted  move to next quetsion
+                            model.nextQuestion()
+                            
+                            // Reset properties
+                            submitted = false
+                            selectedAnswerIndex = nil
+                        }
                         
-                        // Reset properties
-                        submitted = false
-                        selectedAnswerIndex = nil
+                     
                     
                     }
                     else {
@@ -137,9 +147,12 @@ struct TestView: View {
             .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
             
         }
-        else {
+        else if showResults == true {
            // If current question is nil we show the result view
            TestResultView(numCorrect: numCorrect)
+        }
+        else {
+            ProgressView()
         }
         
         
